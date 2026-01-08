e556f6aa950861a5fb02e44e6c9e25750e8206cb
Amit Vikram <amitvikram1151@gmail.com>
2026-01-08 14:49:59 +0530

    day-9

---
+++ test2.java
@@ -0,0 +1,10 @@
+public class test2 {
+    public static void main(String[] args) {
+        String str = "amitvikram";
+        String str1 = "ApnaCollege";
+        String str2 = "amitvikram";
+        System.out.println(str.equals(str1)); // false
+        System.out.println(str.equals(str2)); // true
+        // it check the length and each character one by one of string
+    }
+}
