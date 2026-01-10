ec4a388d075f488d60a900b0c7d15dd87090c462
Amit Vikram <amitvikram1151@gmail.com>
2026-01-10 23:26:37 +0530

    day-11

--- 11_Bit_manipulation/EvenOrOdd.java
+++ 11_Bit_manipulation/EvenOrOdd.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
 public class EvenOrOdd {
+
+    public static int convertBinary(int n ) {
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.close();
+        System.out.print("input the number : ");
+        int n = sc.nextInt();
+
+        System.out.println(convertBinary(n));
+
+
     }
 }
