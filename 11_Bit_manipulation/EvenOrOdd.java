bfcedf17888aef5e56b73388e0b9cf7146382a13
Amit Vikram <amitvikram1151@gmail.com>
2026-01-11 23:19:27 +0530

    day-12

--- 11_Bit_manipulation/EvenOrOdd.java
+++ 11_Bit_manipulation/EvenOrOdd.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class EvenOrOdd {
 
     public static int convertBinary(int n ) {
+        int binaryForm = 0;
+        while(n > 0) {
+            int lastDigit = n % 2;
+            binaryForm = (binaryForm * 10) + lastDigit;
+            n = n / 2;
+        }
+        return binaryForm;
+    }
 
+    public static Boolean checkEvenOrOdd(int binaryForm) {
+       return (binaryForm & 1) == 1;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("input the number : ");
         int n = sc.nextInt();
 
-        System.out.println(convertBinary(n));
+        int collectBinary = convertBinary(n);
+        Boolean result = checkEvenOrOdd(collectBinary);
+        System.out.println(result);
 
 
     }
