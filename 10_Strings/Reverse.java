0cadd7e723e53a450576f808c7f870486ca25556
Amit Vikram <amitvikram1151@gmail.com>
2026-01-25 22:09:28 +0530

    day-18

---
+++ 10_Strings/Reverse.java
@@ -0,0 +1,17 @@
+public class Reverse {
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder("");
+
+        for(int i = str.length()-1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String str = "amit";
+        System.out.println(reverseString(str));
+    }
+}
+
