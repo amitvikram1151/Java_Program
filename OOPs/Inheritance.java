027d0bf9337e87182442eee5fe137d9973f54d7b
Amit Vikram <amitvikram1151@gmail.com>
2026-02-09 11:57:50 +0530

    day-21

--- OOPs/Inheritance.java
+++ OOPs/Inheritance.java
@@ -32,4 +32,3 @@ class Fish extends Animal{
         System.out.println("swims in water");
     }
 }
-//dance of bansti
---
+++ OOPs/Overloading.java
@@ -0,0 +1,19 @@
+public class Overloading {
+public static void main(String[] args) {
+    Calculator calc = new Calculator();
+    System.out.println(calc.sum(2, 5));
+    System.out.println(calc.sum((float)4.2,(float)5.4));
+    System.out.println(calc.sum(2, 4, 6));
+}
+}
+class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+    float sum(float a, float b) {
+        return a + b;
+    }
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
